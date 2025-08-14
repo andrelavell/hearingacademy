a35e5f9f4851dcbb7367550b045720e9715a23b2
Andre Lavell <andrelavell@gmail.com>
2025-08-13 21:17:02 -0700

    config: update site domain from .org to .com and remove trailing slash setting

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://hearingacademy.org',
-  trailingSlash: 'always',
+  site: 'https://hearingacademy.com',
   compressHTML: true,
   build: {
     inlineStylesheets: 'auto'
