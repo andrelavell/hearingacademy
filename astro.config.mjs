325d8340718fc287b111b1476c0e9d4fafbb946e
Andre Lavell <andrelavell@gmail.com>
2025-08-13 21:28:40 -0700

    feat: update domain from hearingacademy.com to hearingacademy.org and add structured data for articles

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://hearingacademy.com',
+  site: 'https://hearingacademy.org',
   compressHTML: true,
   build: {
     inlineStylesheets: 'auto'
